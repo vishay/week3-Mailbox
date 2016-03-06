a9668109eced2d0bce3c82f4af3d63ec1d83790c
Vishay Nihalani <vnihalani@lyft.com>
2016-03-06 00:27:53 -0800

    Initial Commit

---
+++ Mailbox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mailbox
+//
+//  Created by Vishay Nihalani on 3/6/16.
+//  Copyright © 2016 Vishay Nihalani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
